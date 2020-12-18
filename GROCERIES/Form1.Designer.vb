65ae31df5cdd59a275da4dbcbeec6e008f0a2d5f
nrlnadiahh <65320822+nrlnadiahh@users.noreply.github.com>
2020-12-18 23:49:10 +0800

    first commit

---
+++ GROCERIES/Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGroceries
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstDisplay = New System.Windows.Forms.ListBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstDisplay
+        '
+        Me.lstDisplay.Font = New System.Drawing.Font("Trebuchet MS", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDisplay.FormattingEnabled = True
+        Me.lstDisplay.ItemHeight = 29
+        Me.lstDisplay.Location = New System.Drawing.Point(80, 40)
+        Me.lstDisplay.Name = "lstDisplay"
+        Me.lstDisplay.Size = New System.Drawing.Size(373, 323)
+        Me.lstDisplay.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.LightBlue
+        Me.btnCalculate.Font = New System.Drawing.Font("Trebuchet MS", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(102, 399)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(150, 48)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.Color.LightBlue
+        Me.btnClose.Font = New System.Drawing.Font("Trebuchet MS", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.Location = New System.Drawing.Point(276, 399)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(150, 48)
+        Me.btnClose.TabIndex = 2
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'frmGroceries
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 477)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lstDisplay)
+        Me.Name = "frmGroceries"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Groceries Total"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstDisplay As ListBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ GROCERIES/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class frmGroceries
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim objReader As IO.StreamReader
+        Dim objWriter As New IO.StreamWriter("E:\CSC301\result.txt")
+        Dim strLine As String
+        Dim strName As String
+        Dim dblPrice As Double
+        Dim dblTotal As Double
+        Dim strArray() As String
+        If IO.File.Exists("E:\CSC301\groceries.txt") = True Then
+            objReader = IO.File.OpenText("E:\CSC301\groceries.txt")
+        Else
+            MsgBox("File is not available")
+            Close()
+        End If
+
+        Do While objReader.Peek <> -1
+            strLine = objReader.ReadLine()
+            strArray = strLine.Split(",")
+            strName = strArray(0)
+            dblPrice = Convert.ToDouble(strArray(1))
+            dblTotal = dblTotal + dblPrice
+            lstDisplay.Items.Add(strName & " [" & dblPrice.ToString("C") & "]")
+            objWriter.WriteLine(strName & " [" & dblPrice.ToString("C") & "]")
+        Loop
+
+        lstDisplay.Items.Add("Total price of groceries : " & dblTotal.ToString("C"))
+        objWriter.WriteLine("Total price of groceries : " & dblTotal.ToString("C"))
+
+        objReader.Close()
+        objWriter.Close()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Close()
+    End Sub
+
+End Class
